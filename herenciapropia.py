8a0b223ea53808ee9b6d188194b6f49c483586ac
manuelbayonal <87209228+manuelbayonal@users.noreply.github.com>
2022-02-24 09:12:05 -0500

    Delete herenciapropia.py

--- herenciapropia.py
+++
@@ -1,9 +0,0 @@
-class triangulo:
-    def __init__ (self,lado,altura):
-        self.lado=lado
-        self.altura=altura
-    def area(self):
-        return (self.lado*self.altura)/2 
-if __name__=='__main__':
-    triangulo=triangulo(lado=2,altura=4)
-    print("el área del triangulo es: "+str(triangulo.area()))
